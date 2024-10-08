8296a45e2442aebbab7f3d7dca46f6f139176481
scarletstorm <scarletstorm@noreply.codeberg.org>
2024-10-08 19:22:12 +0530

    liella: add host

--- config/homes/violet@marchenstar.nix
+++ config/homes/violet@marchenstar.nix
@@ -2,12 +2,16 @@
   lib,
   config,
   pkgs,
-  homeManagerModules,
+  homeManagerConfig,
   mylib,
   ...
 }:
 
 let
+  f1Kanata2x = pkgs.fetchurl {
+    url = "https://i.idol.st/u/card/art/2x/186Konoe-Kanata-It-s-my-turn-next-UR-92PePZ.png";
+    hash = "sha256-ZsE61EBIXGQBVP899LObmB75no3UA+Ic6aborT4zUJU=";
+  };
   nixglPackage = pkgs.nixgl.override {
     "enable32bits" = false;
     intel-media-driver = pkgs.intel-vaapi-driver;
@@ -18,9 +22,11 @@ in
 {
   imports = [
     ./violet.nix
-    homeManagerModules.plasma
-    homeManagerModules.generic
+    homeManagerConfig.plasma
+    homeManagerConfig.generic
   ];
+  programs.plasma.workspace.wallpaper = "${f1Kanata2x}";
+  programs.plasma.configFile.kdeglobals.General.AccentColor = "166,100,160";
   services.syncthing.enable = true;
   home.packages = [
     (wrapIntelVulkan pkgs.zed-editor "zeditor")
