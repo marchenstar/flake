52c97a6ce44ba1a3e2ed619110ef800a12e006ca
scarletstorm <scarletstorm@noreply.codeberg.org>
2024-10-12 17:00:26 +0530

    Add NetworkManager config

--- secrets/default.nix
+++ secrets/default.nix
@@ -1,4 +1,7 @@
 {
   common = ./common.yaml;
   yharnam = ./yharnam.yaml;
+  # wifi = builtins.map (x: ./pc.yaml/wifi/${x}) (
+  #   builtins.attrNames (builtins.fromJSON (builtins.readFile ./pc.json)).wifi
+  # );
 }
