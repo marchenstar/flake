fa39b9e2a21b7498fd3100bc8d8fa933a788989c
scarletstorm <scarletstorm@noreply.codeberg.org>
2024-11-02 20:47:51 +0530

    aqours: add host

--- config/homes/violet@aqours.nix
+++ config/homes/violet@aqours.nix
@@ -1,7 +1,6 @@
 {
   pkgs,
   homeManagerConfig,
-  mylib,
   ...
 }:
 
@@ -10,26 +9,17 @@ let
     url = "https://i.idol.st/u/card/art/2x/186Konoe-Kanata-It-s-my-turn-next-UR-92PePZ.png";
     hash = "sha256-ZsE61EBIXGQBVP899LObmB75no3UA+Ic6aborT4zUJU=";
   };
-  nixglPackage = pkgs.nixgl.override {
-    "enable32bits" = false;
-    intel-media-driver = pkgs.intel-vaapi-driver;
-  };
-  wrapIntelGL = mylib.wrapPrefix nixglPackage.nixGLIntel;
-  wrapIntelVulkan = mylib.wrapPrefix nixglPackage.nixVulkanIntel;
 in
 {
   imports = [
-    ../violet.nix
+    ./violet.nix
     homeManagerConfig.plasma
-    homeManagerConfig.generic
   ];
   programs.plasma.workspace.wallpaper = "${f1Kanata2x}";
   programs.plasma.configFile.kdeglobals.General.AccentColor = "166,100,160";
   services.syncthing.enable = true;
   home.packages = [
-    (wrapIntelVulkan pkgs.zed-editor "zeditor")
-    (wrapIntelGL pkgs.moonlight-qt "moonlight")
+    pkgs.zed-editor
   ];
-  programs.mpv.package = pkgs.emptyDirectory;
   home.stateVersion = "24.05";
 }
